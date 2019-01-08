c7e42f6748468312021059ac6ab386c302051bd2
JEEU95 <jeeu.95@gmail.com>
2019-01-08 15:02:12 -0500

    2.0

--- src/pages/crear/crear.ts
+++ src/pages/crear/crear.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PeticionesService } from '../../services/peticiones.service';
+import { Persona } from '../../services/persona';
+
 
 /**
  * Generated class for the CrearPage page.
@@ -12,14 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-crear',
   templateUrl: 'crear.html',
+  providers:[ PeticionesService ]
 })
 export class CrearPage {
+  public persona: Persona;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _peticionesService: PeticionesService) {
+    this.persona = new Persona('','','');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CrearPage');
+  onSubmit(form){
+    this._peticionesService.addPersona(this.persona).subscribe(
+      response=>{
+        form.reset();
+        alert(response.respuesta);
+        
+      },
+      error=>{
+        console.log(<any>error);
+      });
   }
 
 }
